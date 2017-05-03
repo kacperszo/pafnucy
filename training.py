fa45b8612133a91615644c4038d07aff546f76ca
martasd <martasd@ibb.waw.pl>
2017-05-03 09:10:46 +0200

    use timestamps in filenames

--- training.py
+++ training.py
@@ -1,5 +1,5 @@
 import numpy as np
-np.random.seed = 123
+np.random.seed(123)
 
 import pandas as pd
 from math import sqrt
@@ -21,10 +21,11 @@ sns.set_context('paper')
 sns.set_color_codes()
 color = {'training': 'b', 'validation': 'g', 'test': 'r'}
 
-
+import time
+timestamp = time.strftime('%Y-%m-%dT%H:%M:%S')
 path = '../pdbbind/v2016/'
-logdir = './logdir/relu/'
-fname = './pdbbind_20a_19f_relu_kp%s_lmb%s_f%s_c%s_lr%s'
+logdir = './logdir/' + timestamp
+fname = './pdbbind_20a_19f_relu_kp%s_lmb%s_f%s_c%s_lr%s_' + timestamp
 vsize = 1000
 grid_spacing = 1.0
 
@@ -34,28 +35,28 @@ print('atomic properties:', data_utils.FEATURE_NAMES)
 columns = {name: i for i, name in enumerate(data_utils.FEATURE_NAMES)}
 
 
-# # uncomment this part to create files with the training and validation sets
-# with h5py.File('%s/training_set.hdf' % path, 'w') as g, \
-#      h5py.File('%s/validation_set.hdf' % path, 'w') as h:
-#     with h5py.File('%s/refined.hdf' % path, 'r') as f:
-#         print('refined')
-#         refined_shuffled = shuffle(list(f.keys()), random_state=123)
-#         for pdb_id in refined_shuffled[:vsize]:
-#             ds = h.create_dataset(pdb_id, data=f[pdb_id])
-#             ds.attrs['affinity'] = f[pdb_id].attrs['affinity']
-#         for pdb_id in refined_shuffled[vsize:]:
-#             ds = g.create_dataset(pdb_id, data=f[pdb_id])
-#             ds.attrs['affinity'] = f[pdb_id].attrs['affinity']
-#     with h5py.File('%s/general.hdf' % path, 'r') as f:
-#         print('general')
-#         for pdb_id in f:
-#             ds = g.create_dataset(pdb_id, data=f[pdb_id])
-#             ds.attrs['affinity'] = f[pdb_id].attrs['affinity']
+# uncomment this part to create files with the training and validation sets
+with h5py.File('%s/training_set.hdf' % path, 'w') as g, \
+     h5py.File('%s/validation_set.hdf' % path, 'w') as h:
+    with h5py.File('%s/refined.hdf' % path, 'r') as f:
+        print('refined')
+        refined_shuffled = shuffle(list(f.keys()), random_state=123)
+        for pdb_id in refined_shuffled[:vsize]:
+            ds = h.create_dataset(pdb_id, data=f[pdb_id])
+            ds.attrs['affinity'] = f[pdb_id].attrs['affinity']
+        for pdb_id in refined_shuffled[vsize:]:
+            ds = g.create_dataset(pdb_id, data=f[pdb_id])
+            ds.attrs['affinity'] = f[pdb_id].attrs['affinity']
+    with h5py.File('%s/general.hdf' % path, 'r') as f:
+        print('general')
+        for pdb_id in f:
+            ds = g.create_dataset(pdb_id, data=f[pdb_id])
+            ds.attrs['affinity'] = f[pdb_id].attrs['affinity']
 
-# # uncomment this part to create a symlink for the test set
-# print('core')
-# import os
-# os.symlink(os.path.abspath('%s/core.hdf' % path), '%s/test_set.hdf' % path)
+# uncomment this part to create a symlink for the test set
+print('core')
+import os
+os.symlink(os.path.abspath('%s/core.hdf' % path), '%s/test_set.hdf' % path)
 
 
 datasets = ['training', 'validation', 'test']
@@ -384,7 +385,7 @@ for set_name, tab in predictions.groupby('set'):
                                              % (set_name, rmse[dataset])})
 
     image = custom_summary_image(grid.fig)
-    grid.fig.savefig(set_name+'.pdf')
+    grid.fig.savefig('%s_%s.pdf' % (set_name, timestamp))
     summary_pred = tf.Summary()
     summary_pred.value.add(tag='predictions_%s' % (set_name),
                            image=image)
